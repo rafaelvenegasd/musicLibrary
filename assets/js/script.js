1d848fca068d81b157db0a2cb1f1137038a76e52
Rafael <arvenegasd@gmail.com>
2019-12-13 17:29:58 +0100

    [display] update

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,14 +7,15 @@ $("#submit").click(function(event){
         dataType: "jsonp",
         url : "https://itunes.apple.com/search?" + "term=" + finder.term + "&" + "country=" + finder.country  + "&" + "explicit=" + finder.explicit  + "&" + "limit=" + finder.limit + "&" + "entity=" + finder.type 
     })
-        
-    .done(function(url) {
+    
+    .done(function() {
+        console.log(datos.responseJSON['results']);
         for (var data of datos.responseJSON['results']) {
             // New Item
-            var itemArtist = new Artist(data['artistName'], data['musicGenre'], data['link']);
+            var itemArtist = new Artist(data['artistName'], data['primaryGenreName'], data['artistLinkUrl']);
             // Container
             var myNode = document.createElement('div');
-            myNode.classList.add('card', 'col-md-3', 'ml-1', 'mb-2');
+            myNode.classList.add('card', 'col-md-2', 'ml-1', 'mb-2');
             // Body of the container
             var myNodeCardBody = document.createElement('div');
             myNodeCardBody.classList.add('card-body');

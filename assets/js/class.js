21363af181f1fa2b83e27577b2f79f387599014c
Rafael <arvenegasd@gmail.com>
2019-12-13 14:04:52 +0100

    getting data from the API

---
+++ assets/js/class.js
@@ -0,0 +1,63 @@
+// class Library { 
+//     constructor(content){
+//         this.content = content;
+//     }
+// }
+
+// class Artist {
+//     constructor(artistName, musicGenre, link){
+//         this.artistName = artistName;
+//         this.musicGenre = musicGenre;
+//         this.link = link;
+//     }
+// }
+
+// class Album extends Artist {
+//     constructor(albumName, caratula, artistName, price, numSongs, creationDate, musicGenre){
+//         super(artistName);
+//             this.albumName = albumName;
+//             this.caratula = caratula;
+//             this.price = price;
+//             this.numSongs = numSongs;
+//             this.creationDate = creationDate;
+//             this.musicGenre = musicGenre;
+//     }
+// }
+
+// class Song extends (Artist, Album) {
+//     constructor(songName, caratula, artistName, albumName, price, lenght, creationDate, musicGenre, audio, link){
+//         super(artistName, albumName);
+//             this.songName = songName;
+//             this.caratula = caratula;
+//             this.price = price;
+//             this.lenght = lenght;
+//             this.creationDate = creationDate;
+//             this.musicGenre = musicGenre;
+//             this.audio = audio;
+//             this.link = link;
+//     }
+// }
+
+
+// class MusicVideo extends (Artist, Song) {
+//     constructor(musicVideoName, caratula, artistName, songName, lenght, creationDate, musicGenre, video, link){
+//         super(artistName, songName);
+//             this.musicVideoName = musicVideoName;
+//             this.caratula = caratula;
+//             this.lenght = lenght;
+//             this.creationDate = creationDate;
+//             this.musicGenre = musicGenre;
+//             this.video = video;
+//             this.link = link;
+//     }
+// }
+
+class Search{
+    constructor(term, country, limit, explicit, type){
+        this.term = term;
+        this.country = country;
+        this.limit = limit;
+        this.explicit = explicit;
+        this.type = type;
+    }
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
-$(document).ready(function(){
-    alert("Hola mundo");
+
+
+
+$("#submit").click(function(){
+    event.preventDefault();
+    var finder = new Search($("#inputSearch").val(), $("#country").val(), $("#limit").val(), $("#explicit").val(), $("#type").val()); 
+    $.ajax(
+        {
+        type: "GET",
+        dataType: "jsonp",
+        url : "https://itunes.apple.com/search?" + "term=" + finder.term + "&" + "country=" + finder.country  + "&" + "explicit=" + finder.explicit  + "&" + "limit=" + finder.limit + "&" + "entity=" + finder.type 
+    })
+        
+    .done(function(url) {
+    console.log(url);
+    })
+    .fail(function() {
+    console.log("error");
+    });
 })
+
+

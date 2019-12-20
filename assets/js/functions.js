8a332686bdfba46df3107cf744bf66571c03b096
Rafael <arvenegasd@gmail.com>
2019-12-20 10:32:25 +0100

    [files structure] update

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -65,5 +65,10 @@ $.ajax({
     console.log(error);
 });
 
+//add options in the selector of limit
+for (let i = 1; i <= 200; i++) {
+    $("#limit").append("<option value='"+i+"'>"+i+"</option>");   
+}
+
 
 
--- assets/js/objects.js
+++
@@ -1,4 +0,0 @@
-//add options in the selector of limit
-for (let i = 1; i <= 200; i++) {
-    $("#limit").append("<option value='"+i+"'>"+i+"</option>");   
-}
